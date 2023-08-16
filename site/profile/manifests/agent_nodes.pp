91661a5e6ddabf563b2661713255e9b3b7c70d8d
Deshan Ranathunga <deshan.99.ranathunga@gmail.com>
2023-08-16 17:59:09 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,2 +1,10 @@
 class profile::agent_nodes {
+  host {'web.puppet.vm':
+    ensure => present,
+    ip     => '10.0.0.11',
+  }
+  host {'db.puppet.vm':
+    ensure => present,
+    ip     => '10.0.0.12',
+  }
 }
